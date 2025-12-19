17b38adedc9da7333bd2e1893a3b5efe3ef45a62
admrynr <reyneradam@gmail.com>
2025-12-20 05:33:28 +0700

    edit config vite

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    react(),
-  ],
-
+  plugins: [react(), tailwindcss()],
   server: {
-    port: 3000,
-    strictPort: true,
-  },
-
-  preview: {
-    port: 3000,
-  },
-});
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true
+      }
+    }
+  }
+})
